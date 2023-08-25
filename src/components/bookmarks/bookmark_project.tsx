9fdf58d86d6ba2b8daa43480f3db9537bbe2812f
Pratham-Mishra04 <prathamm0409@gmail.com>
2023-08-25 12:32:54 +0530

    feat: lower project added

---
+++ src/components/bookmarks/bookmark_project.tsx
@@ -0,0 +1,115 @@
+import postHandler from '@/handlers/post_handler';
+import { setProjectBookmarks, userSelector } from '@/slices/userSlice';
+import { Project, ProjectBookmark, ProjectBookmarkItem } from '@/types';
+import Toaster from '@/utils/toaster';
+import React, { FormEvent, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { BOOKMARK_URL } from '@/config/routes';
+
+interface Props {
+  setShow: React.Dispatch<React.SetStateAction<boolean>>;
+  project: Project;
+  setBookmark: (isBookmarked: boolean, projectItemID: string, bookmarkID: string) => void;
+}
+
+const BookmarkProject = ({ setShow, project, setBookmark }: Props) => {
+  const [bookmarkTitle, setBookmarkTitle] = useState('');
+  const [mutex, setMutex] = useState(false);
+  const dispatch = useDispatch();
+
+  const bookmarks = useSelector(userSelector).projectBookmarks;
+
+  useEffect(() => {
+    document.documentElement.style.overflowY = 'hidden';
+    document.documentElement.style.height = '100vh';
+
+    return () => {
+      document.documentElement.style.overflowY = 'auto';
+      document.documentElement.style.height = 'auto';
+    };
+  }, []);
+
+  const addBookmarkHandler = async (el: FormEvent<HTMLFormElement>) => {
+    el.preventDefault();
+    if (mutex) return;
+    setMutex(true);
+    const toaster = Toaster.startLoad('Adding your Bookmark...');
+
+    const URL = `${BOOKMARK_URL}/project`;
+    const res = await postHandler(URL, { title: bookmarkTitle });
+
+    if (res.statusCode === 201) {
+      const bookmark: ProjectBookmark = res.data.bookmark;
+      const updatedBookmarks = [...bookmarks, bookmark];
+      dispatch(setProjectBookmarks(updatedBookmarks));
+      Toaster.stopLoad(toaster, 'Bookmark Added', 1);
+      setBookmarkTitle('');
+    } else {
+      if (res.data.message) Toaster.stopLoad(toaster, res.data.message, 0);
+      else {
+        Toaster.stopLoad(toaster, 'Internal Server Error', 0);
+        console.log(res);
+      }
+    }
+    setMutex(false);
+  };
+
+  const addBookmarkItemHandler = async (bookmark: ProjectBookmark) => {
+    if (mutex) return;
+    setMutex(true);
+
+    const URL = `${BOOKMARK_URL}/project/item/${bookmark.id}`;
+    const res = await postHandler(URL, { projectID: project.id });
+
+    if (res.statusCode === 201) {
+      const bookmarkItem: ProjectBookmarkItem = res.data.bookmarkItem;
+      const updatedBookmarks = bookmarks.map(projectBookmark => {
+        if (projectBookmark.id === bookmark.id) {
+          const updatedPostItems = projectBookmark.projectItems
+            ? [...projectBookmark.projectItems, bookmarkItem]
+            : [bookmarkItem];
+          return { ...projectBookmark, postItems: updatedPostItems };
+        }
+        return projectBookmark;
+      });
+      dispatch(setProjectBookmarks(updatedBookmarks));
+      setShow(false);
+      setBookmark(true, bookmarkItem.id, bookmarkItem.projectBookmarkID);
+    }
+    setMutex(false);
+  };
+  return (
+    <>
+      <div className="fixed top-12 w-1/3 max-md:w-5/6 h-max bg-slate-100 right-1/2 translate-x-1/2 animate-fade_third z-20">
+        <div>BookMark this Project</div>
+        {bookmarks.map((bookmark, index: number) => {
+          return (
+            <div
+              key={index}
+              className={`w-full h-14 flex justify-center rounded-xl rounded-b-none items-center border-b-2 border-black hover:bg-[#1a1a1a18] cursor-pointer`}
+              onClick={() => {
+                addBookmarkItemHandler(bookmark);
+              }}
+            >
+              {bookmark.title}
+            </div>
+          );
+        })}
+        <form onSubmit={addBookmarkHandler}>
+          <input
+            className={`w-full h-14 px-2 flex rounded-xl justify-center items-center font-Helvetica bg-[#1a1a1a18] cursor-pointer focus:outline-none`}
+            value={bookmarkTitle}
+            onChange={el => setBookmarkTitle(el.target.value)}
+            placeholder="Create a new Bookmark"
+          />
+        </form>
+      </div>
+      <div
+        onClick={() => setShow(false)}
+        className=" bg-backdrop w-screen h-screen fixed top-0 right-0 animate-fade_third z-10"
+      ></div>
+    </>
+  );
+};
+
+export default BookmarkProject;
---
+++ src/components/lowers/lower_project.tsx
@@ -0,0 +1,192 @@
+import React, { useState, useEffect } from 'react';
+import { Project, ProjectBookmark } from '@/types';
+import Cookies from 'js-cookie';
+import deleteHandler from '@/handlers/delete_handler';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import getHandler from '@/handlers/get_handler';
+import { useDispatch, useSelector } from 'react-redux';
+import { setLikes, setProjectBookmarks, userSelector } from '@/slices/userSlice';
+// import clickedOnSharePost from './clickedOnShare_project';
+import BookmarkSimple from '@phosphor-icons/react/dist/icons/BookmarkSimple';
+import Gear from '@phosphor-icons/react/dist/icons/Gear';
+import Export from '@phosphor-icons/react/dist/icons/Export';
+import Heart from '@phosphor-icons/react/dist/icons/Heart';
+import ChatTeardrop from '@phosphor-icons/react/dist/icons/ChatTeardrop';
+import BookmarkProject from '../bookmarks/bookmark_project';
+import { BOOKMARK_URL, POST_URL } from '@/config/routes';
+import Semaphore from '@/utils/semaphore';
+import { configSelector, setFetchingLikes } from '@/slices/configSlice';
+
+interface Props {
+  project: Project;
+}
+
+interface bookMarkStatus {
+  isBookmarked: boolean;
+  projectItemID: string;
+  bookmarkID: string;
+}
+
+const LowerProject = ({ project }: Props) => {
+  const [liked, setLiked] = useState(false);
+  const [numLikes, setNumLikes] = useState(project.noLikes);
+  const [bookmarkStatus, setBookmarkStatus] = useState<bookMarkStatus>({
+    isBookmarked: false,
+    projectItemID: '',
+    bookmarkID: '',
+  });
+  const [clickedOnShare, setClickedOnShare] = useState(false);
+  const [clickedOnBookmark, setClickedOnBookmark] = useState(false);
+  const [mutex, setMutex] = useState(false);
+
+  const likes = useSelector(userSelector).likes;
+  const bookmarks = useSelector(userSelector).projectBookmarks;
+
+  const userID = Cookies.get('id');
+
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const updatingLikes = useSelector(configSelector).fetchingLikes;
+
+  const semaphore = new Semaphore(updatingLikes, setFetchingLikes);
+
+  const setBookmark = (isBookmarked: boolean, projectItemID: string, bookmarkID: string) => {
+    setBookmarkStatus({
+      isBookmarked,
+      projectItemID,
+      bookmarkID,
+    });
+  };
+
+  function removePostItemFromBookmark(
+    bookmarks: ProjectBookmark[],
+    bookmarkId: string,
+    projectItemId: string
+  ): ProjectBookmark[] {
+    const updatedBookmarks = bookmarks.map(bookmark => {
+      if (bookmark.id === bookmarkId) {
+        const updatedPostItems = bookmark.projectItems.filter(item => item.id !== projectItemId);
+        return { ...bookmark, projectItems: updatedPostItems };
+      }
+      return bookmark;
+    });
+
+    return updatedBookmarks;
+  }
+
+  useEffect(() => {
+    if (likes.includes(project.id)) setLiked(true);
+    bookmarks.forEach(bookmarksObj => {
+      if (bookmarksObj.projectItems)
+        bookmarksObj.projectItems.forEach(projectItem => {
+          if (projectItem.projectID == project.id) setBookmark(true, projectItem.id, bookmarksObj.id);
+        });
+    });
+  }, []);
+
+  const likeHandler = async () => {
+    await semaphore.acquire();
+
+    if (liked) setNumLikes(prev => prev - 1);
+    else setNumLikes(prev => prev + 1);
+
+    setLiked(prev => !prev);
+
+    const URL = `${POST_URL}/like/${project.id}`;
+    const res = await getHandler(URL);
+
+    if (res.statusCode === 200) {
+      const newLikes: string[] = [...likes];
+      if (liked) {
+        newLikes.splice(newLikes.indexOf(project.id), 1);
+      } else {
+        newLikes.push(project.id);
+      }
+      dispatch(setLikes(newLikes));
+    } else {
+      if (liked) setNumLikes(prev => prev + 1);
+      else setNumLikes(prev => prev - 1);
+      setLiked(prev => !prev);
+    }
+
+    semaphore.release();
+  };
+
+  const removeBookmarkItemHandler = async () => {
+    if (mutex) return;
+    setMutex(true);
+    setBookmark(false, bookmarkStatus.projectItemID, bookmarkStatus.bookmarkID);
+
+    const URL = `${BOOKMARK_URL}/project/item/${bookmarkStatus.projectItemID}`;
+    const res = await deleteHandler(URL);
+    if (res.statusCode === 204) {
+      const updatedBookmarks = removePostItemFromBookmark(
+        bookmarks,
+        bookmarkStatus.bookmarkID,
+        bookmarkStatus.projectItemID
+      );
+      dispatch(setProjectBookmarks(updatedBookmarks));
+      setBookmark(false, '', '');
+    } else {
+      setBookmark(true, bookmarkStatus.projectItemID, bookmarkStatus.bookmarkID);
+    }
+    setMutex(false);
+  };
+
+  return (
+    <>
+      {/* {clickedOnShare ? <clickedOnShareProject id={project.id} setShow={setClickedOnShare} /> : <></>} */}
+      {clickedOnBookmark ? (
+        <BookmarkProject setShow={setClickedOnBookmark} project={project} setBookmark={setBookmark} />
+      ) : (
+        <></>
+      )}
+      <div className="w-full flex justify-between pl-14">
+        <div className="flex gap-6 max-md:gap-3">
+          <div onClick={likeHandler} className="flex items-center gap-2">
+            <Heart className="cursor-pointer max-md:w-6 max-md:h-6" size={40} weight={liked ? 'duotone' : 'regular'} />
+            <div className="">{numLikes}</div>
+          </div>
+          <Link className="flex items-center gap-2" href={`/explore/project/comments/${project.id}`}>
+            <ChatTeardrop className="cursor-pointer max-md:w-[32px] max-md:h-[32px]" size={40} weight="duotone" />
+            <div className="">{project.noComments}</div>
+          </Link>
+          <div className="flex items-center gap-2" onClick={() => setClickedOnShare(true)}>
+            <Export className="cursor-pointer max-md:w-[32px] max-md:h-[32px]" size={40} weight="duotone" />
+            <div className="">{project.noShares}</div>
+          </div>
+        </div>
+        <div className="relative">
+          <div className="flex gap-2">
+            {userID == project?.userID ? (
+              <Gear
+                className="cursor-pointer max-md:w-[32px] max-md:h-[32px]"
+                onClick={() => {
+                  router.push(`/workspace/project/edit/${project.id}`);
+                }}
+                size={40}
+                weight="light"
+              />
+            ) : (
+              <></>
+            )}
+
+            <BookmarkSimple
+              className="cursor-pointer max-md:w-[32px] max-md:h-[32px]"
+              onClick={() => {
+                if (bookmarkStatus.isBookmarked) removeBookmarkItemHandler();
+                else setClickedOnBookmark(prev => !prev);
+              }}
+              size={40}
+              weight={bookmarkStatus.isBookmarked ? 'duotone' : 'light'}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LowerProject;
