889f0b770224a7190adf9305bf0281dbc2f528c4
manaslaud <manaslaud@hotmail.com>
2023-12-15 23:37:12 +0530

    fix: added missing sections  of deleted component

--- src/components/organisation_history/created.tsx
+++ src/components/organisation_history/created.tsx
@@ -10,28 +10,28 @@ const Created = ({ history }: Props) => {
   switch (history.HistoryType) {
    case -1:
    return ( < OrganizationHistoryWrapper history={history}>
-    <div className="w-fit text-center flex-center gap-4">Created {history.OrganizationID}! 🎉</div>
+    <div className="w-fit text-center flex-center gap-4">Created new organisation: {history.OrganizationID}! 🎉</div>
   </  OrganizationHistoryWrapper>)
   case 0:
     return (< OrganizationHistoryWrapper history={history}>
-      <div className="w-fit text-center flex-center gap-4">Created {history.Event?.title} 🎉</div>
+      <div className="w-fit text-center flex-center gap-4">Created new event: {history.Event?.title} 🎉</div>
     </  OrganizationHistoryWrapper>)
   case 3:
    return ( < OrganizationHistoryWrapper history={history}>
-    <div className="w-fit text-center flex-center gap-4">{history.Invitation?.user.id}🎉</div>
+    <div className="w-fit text-center flex-center gap-4">Invited new user: {history.Invitation?.user.id}🎉</div>
   </  OrganizationHistoryWrapper>)
 
   case 6:
    return ( < OrganizationHistoryWrapper history={history}>
-    <div className="w-fit text-center flex-center gap-4">{history.Post?.content} 🎉</div>
+    <div className="w-fit text-center flex-center gap-4">Created a post: {history.Post?.content} 🎉</div>
   </  OrganizationHistoryWrapper>)
   case 9:
     return (< OrganizationHistoryWrapper history={history}>
-      <div className="w-fit text-center flex-center gap-4">{history.Project?.description} 🎉</div>
+      <div className="w-fit text-center flex-center gap-4">Created a project:{history.Project?.description} 🎉</div>
     </  OrganizationHistoryWrapper>)
   case 12:
     return (< OrganizationHistoryWrapper history={history}>
-      <div className="w-fit text-center flex-center gap-4">{history.Task?.description} 🎉</div>
+      <div className="w-fit text-center flex-center gap-4">Created a task:{history.Task?.description} 🎉</div>
     </  OrganizationHistoryWrapper>)
 
     default:
--- src/components/organisation_history/deleted.tsx
+++ src/components/organisation_history/deleted.tsx
@@ -11,7 +11,37 @@ const Deleted = ({ history }: Props) => {
     case 1: 
       return (
         <OrganizationHistoryWrapper history={history}>
-          <div className="w-fit text-center flex-center gap-4">deleted an Opening.</div>
+          <div className="w-fit text-center flex-center gap-4">Deleted an event: {history.Event?.description}.</div>
+        </OrganizationHistoryWrapper>
+      );
+      case 4: 
+      return (
+        <OrganizationHistoryWrapper history={history}>
+          <div className="w-fit text-center flex-center gap-4">Withdrew an invitation:{history.Invitation?.user.id}</div>
+        </OrganizationHistoryWrapper>
+      );
+      case 5: 
+      return (
+        <OrganizationHistoryWrapper history={history}>
+          <div className="w-fit text-center flex-center gap-4">Removed a member: {history.User.id}.</div>
+        </OrganizationHistoryWrapper>
+      );
+      case 7: 
+      return (
+        <OrganizationHistoryWrapper history={history}>
+          <div className="w-fit text-center flex-center gap-4">Deleted a post: {history.Post?.content}.</div>
+        </OrganizationHistoryWrapper>
+      );
+      case 10: 
+      return (
+        <OrganizationHistoryWrapper history={history}>
+          <div className="w-fit text-center flex-center gap-4">Deleted a project: {history.Project?.description}.</div>
+        </OrganizationHistoryWrapper>
+      );
+      case 13: 
+      return (
+        <OrganizationHistoryWrapper history={history}>
+          <div className="w-fit text-center flex-center gap-4">Deleted a task: {history.Task?.description}</div>
         </OrganizationHistoryWrapper>
       );
     default:
